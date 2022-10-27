c4c238c6aa05b12d49b753f496e71792f35a043a
2W2-PalaciosLucio-112035 <112035@tecnicatura.frc.utn.edu.ar>
2022-10-27 19:59:49 -0300

    refinando detalles

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -32,14 +32,15 @@ export class RegistroComponent implements OnInit {
 
   registrar() {
     if (!this.formRegister.valid)
-      return;
-
-    this.subscription.add(
-      this.servicio.registrarUsuario(this.formRegister.value).subscribe({
-        next: (result) => { this.mensajeExitoso("Bienvenido", "Gracias por registrarte"); console.log(result); this.router.navigateByUrl("/login") },
-        error: (error) => { this.mensajeError(error.error, "Error"); console.log(error.error), this.formRegister.reset() }
-      })
-    )
+      return this.mensajeError("Datos invalidos", "Invalido");
+    else {
+      this.subscription.add(
+        this.servicio.registrarUsuario(this.formRegister.value).subscribe({
+          next: (result) => { this.mensajeExitoso("Bienvenido", "Gracias por registrarte"); console.log(result); this.router.navigateByUrl("/login") },
+          error: (error) => { this.mensajeError(error.error, "Error"); console.log(error.error), this.formRegister.reset() }
+        })
+      )
+    }
   }
   volver() {
     this.router.navigateByUrl("/login")
@@ -48,18 +49,18 @@ export class RegistroComponent implements OnInit {
   mensajeError(errorMessage: string, title: string): void {
     Swal.fire({
       icon: 'error',
-      title: title,color:'#343a40',
+      title: title, color: '#343a40',
       text: errorMessage,
-      confirmButtonColor:'#dc3545'
-    
+      confirmButtonColor: '#dc3545'
+
     });
   }
   mensajeExitoso(title: string, text: string): void {
     Swal.fire({
       icon: 'success',
-      title: title,color:'#343a40',
+      title: title, color: '#343a40',
       text: text,
-      confirmButtonColor:'#198754',
+      confirmButtonColor: '#198754',
       timer: 5000
     });
   }
